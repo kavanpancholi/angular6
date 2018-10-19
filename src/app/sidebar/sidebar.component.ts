9f006d7be43d862d7fc6b0ee1577edb361156198
kavan <kavanpancholi@gmail.com>
2018-10-20 00:48:49 +0530

    Add current route class for active pages

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
+    selector: 'app-sidebar',
+    templateUrl: './sidebar.component.html',
 
-  styleUrls: ['./sidebar.component.scss', '../../../node_modules/font-awesome/scss/font-awesome.scss']
+    styleUrls: ['./sidebar.component.scss', '../../../node_modules/font-awesome/scss/font-awesome.scss']
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+    currentUrl: String;
 
-  ngOnInit() {
-  }
+    constructor(private router: Router) {
+        // this.currentUrl = router.url;
+        router.events.subscribe((_: NavigationEnd) => this.currentUrl = this.router.url);
+    }
+
+    ngOnInit() {
+    }
 
 }
